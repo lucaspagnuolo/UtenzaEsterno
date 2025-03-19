9cee4612e107dc9932eacf8f751907d004846322
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-03-19 12:42:31 +0100

    UtenzaEsterno.py
    versione finale

--- UtenzaEsterno.py
+++ UtenzaEsterno.py
@@ -20,10 +20,7 @@ cognome = st.text_input("Cognome").strip().capitalize()
 numero_telefono = st.text_input("Numero di Telefono", "").replace(" ", "")
 expire_date = st.text_input("Data di Fine (gg-mm-aaaa)", "30-06-2025")
 
-# Variabile di input per Description con valore di default <PC>
 description_input = st.text_input("Description (lascia vuoto per <PC>)", "<PC>").strip()
-
-# Variabile per il codice fiscale per tutti gli utenti
 codice_fiscale = st.text_input("Codice Fiscale", "").strip()
 
 if tipo_utente == "Dipendente Consip":
@@ -38,45 +35,41 @@ else:
     ou = "Utenti esterni - Consulenti" if dipendente == "Consulente" else "Utenti esterni - Somministrati e Stage"
     employee_id = ""
     department = "Utente esterno"
-    inserimento_gruppo = "consip_vpn"
+    if dipendente == "Somministrato/Stage":
+        inserimento_gruppo = "consip_vpn;dipendenti_wifi;mobile_wifi;GEDOGA-P-DOCGAR;GRPFreeDeskUser"
+    else:
+        inserimento_gruppo = "consip_vpn"
     telephone_number = ""
     company = ""
 
-# Utilizza il codice fiscale come employeeNumber
 employee_number = codice_fiscale
 
 if st.button("Genera CSV"):
-    # Creazione valori comuni
     sAMAccountName = f"{nome.lower()}.{cognome.lower()}" if tipo_utente == "Dipendente Consip" else f"{nome.lower()}.{cognome.lower()}.ext"
     display_name = f"{cognome} {nome} (esterno)" if tipo_utente == "Esterno" else f"{cognome} {nome}"
     cn = display_name
     expire_date_formatted = formatta_data(expire_date)
     userprincipalname = f"{sAMAccountName}@consip.it"
     mobile = f"+39 {numero_telefono}" if numero_telefono else ""
-
-    # Se la variabile Description è vuota, imposta il valore di default a <PC>
     description = description_input if description_input else "<PC>"
 
-    # Riga CSV
     row = [
         sAMAccountName, "SI", ou, sAMAccountName, display_name, cn, nome, cognome,
         employee_number, employee_id, department, description, "No", expire_date_formatted,
         userprincipalname, userprincipalname, mobile, "", inserimento_gruppo, "", "", telephone_number, company
     ]
 
-    # Creazione del file CSV in memoria (senza salvarlo subito su disco)
     output = io.StringIO()
     writer = csv.writer(output, quoting=csv.QUOTE_MINIMAL)
-    writer.writerow([ 
+    writer.writerow([
         "sAMAccountName", "Creation", "OU", "Name", "DisplayName", "cn", "GivenName", "Surname",
         "employeeNumber", "employeeID", "department", "Description", "passwordNeverExpired",
         "ExpireDate", "userprincipalname", "mail", "mobile", "RimozioneGruppo", "InserimentoGruppo",
         "disable", "moveToOU", "telephoneNumber", "company"
     ])
     writer.writerow(row)
-    output.seek(0)  # Resetta il puntatore per leggere il contenuto
+    output.seek(0)
 
-    # Mostra i risultati in un dataframe
     df = pd.DataFrame([row], columns=[
         "sAMAccountName", "Creation", "OU", "Name", "DisplayName", "cn", "GivenName", "Surname",
         "employeeNumber", "employeeID", "department", "Description", "passwordNeverExpired",
@@ -85,7 +78,6 @@ if st.button("Genera CSV"):
     ])
     st.dataframe(df)
 
-    # Aggiungi un pulsante per scaricare il file CSV generato
     st.download_button(
         label="Scarica il CSV",
         data=output.getvalue(),
